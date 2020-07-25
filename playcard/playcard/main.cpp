a25a6822075bfe1f75cb3f0fc35413a8249dfbfe
guan <hughxu@HughdeMacBook-Pro.local>
2020-07-25 19:20:40 +0800

    test

---
+++ playcard/playcard/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  playcard
+//
+//  Created by guan on 2020/7/25.
+//  Copyright © 2020 guan. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
